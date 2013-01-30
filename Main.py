8684ed486f532719e1399858250f71fa83bfcca7
Thang-Le <leducthang290193@yahoo.com>
2013-01-30 16:47:21 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,14 +2,13 @@ import calculator
 import mess
 from tkinter import *
 
-<<<<<<< HEAD
 print("enter numders that you want to combine")
 so1 = input("first number")
 so2 = input("second number")
 cal = calculator.calculate(so1,so2)
 calcifer = cal.display()
 mess.mss(calcifer)
-=======
+
 done = True
 while done == True:
     print("enter numders that you want to combine")
@@ -22,5 +21,4 @@ while done == True:
     if(str(answer)=='y'):
         done = True
     else: done = False
->>>>>>> new update
 
